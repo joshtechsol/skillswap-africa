609288f247225d6bae4ab316b7678a1f67e019d2
joshtechsol <njorogejoshua36@gmail.com>
2025-07-01 23:16:47 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,13 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB error:", err));
 const express = require("express");
 const app = express();
 const cors = require("cors");
